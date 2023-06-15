f24c146a565767124537cca3415d85bb6c832a1d
karanja03 <wambuik03@gmail.com>
2023-06-15 13:52:22 +0300

    Completed arrow functions

---
+++ index.js
@@ -0,0 +1,13 @@
+const divide = function(){
+    let num1 = 2000;
+    let num2 = 100;
+    return num1 / num2;
+   
+};
+divide();
+
+const square =(num3) => (num3 ** 2);
+square(2);
+
+const add = (num4, num5) => (num4 + num5);
+add(3,4);
